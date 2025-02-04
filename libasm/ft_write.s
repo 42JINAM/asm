66a7dd1d0216ae6448bb7d7b19a53362824d622b
42JINAM <jinam@student.42.fr>
2025-02-04 16:25:19 +0100

    ft_write

--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -3,6 +3,7 @@
 	; 2nd argument  rsi
 	; 3rd argument rdx
 
+	extern  __errno_location
 	section .data
 	SYS_WRITE db 0
 
@@ -10,3 +11,15 @@
 	global  ft_write
 
 ft_write:
+	mov rax, 1
+	syscall
+	cmp rax, 0
+	jl  error_handle
+	ret
+
+error_handle:
+	neg rax; - => +, + => -; errornumber
+	mov r8, rax
+	mov [rax], r8
+	mov rax, -1
+	ret
