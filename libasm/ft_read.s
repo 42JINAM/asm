5fc7fd6137c9d4a70db73bf5dc0e6a37f9f2604a
42JINAM <jinam@student.42.fr>
2025-02-04 16:25:10 +0100

    ft_read

---
+++ libasm/ft_read.s
@@ -0,0 +1,25 @@
+	;ssize_t read(int fd, const void *buf, size_t count)
+	; 1st argument rdi
+	; 2nd argument  rsi
+	; 3rd argument rdx
+
+	extern  __errno_location
+	section .data
+	SYS_WRITE db 0
+
+	section .text
+	global  ft_read
+
+ft_read:
+	mov rax, 0
+	syscall
+	cmp rax, 0
+	jl  error_handle
+	ret
+
+error_handle:
+	neg rax; - => +, + => -; errornumber
+	mov r8, rax
+	mov [rax], r8
+	mov rax, -1
+	ret
