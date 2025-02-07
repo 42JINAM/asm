8be70fb5c2dd6ca41380750dab5b07daf8dc4abb
42JINAM <jinam@student.42.fr>
2025-02-07 16:39:12 +0100

    ft_strdup

--- libasm/libasm.h
+++ libasm/libasm.h
@@ -7,4 +7,5 @@ char * ft_strcpy(char *dst, const char *src);
 int ft_strcmp(const char *s1, const char *s2);
 ssize_t ft_write(int fd, const void *str, int len);
 ssize_t ft_read(int fd, void *buf, size_t count);
+char *ft_strdup(const char *src);
 #endif
